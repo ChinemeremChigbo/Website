14c58d69afae44295487cad58f49d6e907aa779b
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-12 02:03:25 -0400

    unsereted

--- src/config/config.tsx
+++
@@ -1,9 +0,0 @@
-import { SECRETS } from './secrets';
-
-export const FIREBASE_CONFIG = {
-  apiKey: SECRETS.FIREBASE.API_KEY,
-  authDomain: SECRETS.FIREBASE.AUTH_DOMAIN,
-  databaseURL: SECRETS.FIREBASE.DATABASE_URL,
-  storageBucket: SECRETS.FIREBASE.STORAGE_BUCKET,
-  messagingSenderId: SECRETS.FIREBASE.MESSAGING_SENDER_ID,
-};
