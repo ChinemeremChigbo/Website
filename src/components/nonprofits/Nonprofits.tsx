617a46eb4a32ef6c82121abe6466be7aeff8774c
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-13 18:23:57 -0400

    background change

--- src/components/nonprofits/Nonprofits.tsx
+++ src/components/nonprofits/Nonprofits.tsx
@@ -3,16 +3,5 @@ import './Nonprofits.scss';
 
 export default function Nonprofits() {
   //Temporary inline styling. Should be changed when real code is added
-  return (
-    <div
-      style={{
-        background: 'url("../../assets/Background.png") fixed',
-        backgroundSize: 'cover',
-      }}
-      className="nonprofits"
-    >
-      {' '}
-      For nonprofits
-    </div>
-  );
+  return <div className="nonprofits"> For nonprofits</div>;
 }
