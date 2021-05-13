c15af4fcd06405a7ee782234319001de3f7f8945
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-13 14:41:54 -0400

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,10 +4,7 @@ import './Home.scss';
 export default function Home() {
   //Temporary inline styling. Should be changed when real code is added
   return (
-    <div
-      className="home"
-      style={{ position: 'absolute', top: 100, left: 50 }}
-    >
+    <div className="home" style={{ position: 'absolute', top: 100, left: 50 }}>
       Welcome!
     </div>
   );
