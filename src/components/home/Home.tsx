86d3af17a188daf6ddde5bff2c4f29da1c925cd7
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-13 16:13:50 -0400

    Revert "Update Home.tsx"
    This reverts commit c15af4fcd06405a7ee782234319001de3f7f8945.

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,7 +4,10 @@ import './Home.scss';
 export default function Home() {
   //Temporary inline styling. Should be changed when real code is added
   return (
-    <div className="home" style={{ position: 'absolute', top: 100, left: 50 }}>
+    <div
+      className="home"
+      style={{ position: 'absolute', top: 100, left: 50 }}
+    >
       Welcome!
     </div>
   );
