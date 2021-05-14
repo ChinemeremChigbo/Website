c3889e1d73a8d8688eac5e72140ec6822c73e7ad
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-14 05:12:36 -0400

    nav changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+document.onkeydown = function (e) {
+  console.log('key down');
+  console.log(e);
+};
 
 const Navbar = () => {
   return (
     //SVG of Blueprint Logo and Name
     <div className="background">
-      <audio controls autoPlay loop preload="auto">
+      <audio id="myAudio" controls autoPlay loop preload="auto">
         <source src="BackgroundMusic.ogg" type="audio/ogg" />
         <source src="BackgroundMusic.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
