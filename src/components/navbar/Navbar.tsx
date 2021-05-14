2f4d5ad85ef08832c5730e50a83c2aeb753b73ba
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-14 03:59:39 -0400

    added audio

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,6 +6,11 @@ const Navbar = () => {
   return (
     //SVG of Blueprint Logo and Name
     <div className="background">
+      <audio controls autoPlay loop preload="auto">
+        <source src="BackgroundMusic.ogg" type="audio/ogg" />
+        <source src="BackgroundMusic.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
       <img className="name" src="Name.svg" alt="Chinemerem Chigbo"></img>
       <img className="name" src="PressEnter.png" alt="Press Enter"></img>
     </div>
