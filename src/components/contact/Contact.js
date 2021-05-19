0df32fde7cf1d821564613704b70e525e55f395e
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-19 15:21:22 -0400

    contact sizing fixes

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -17,57 +17,36 @@ function Controls(props) {
 
             <img className={props.isSettings ? 'settingsOn' : 'settingsOff'} src="Contact.png" alt="Experience"></img>
             <div className="contactGrid">
-              <div className="grid-item"></div>
-              <div className="grid-item"></div>
-              <div className="grid-item">
-                {' '}
-                <button
-                  aria-label="Devpost"
-                  className={props.isSettings ? 'settingsDevpost' : 'settingsDevpostOff'}
-                  onClick={() => window.open('https://devpost.com/ChinemeremChigbo', '_blank')}
-                ></button>
-              </div>
-              <div className="grid-item">
-                <button
-                  aria-label="Email"
-                  className={props.isSettings ? 'settingsEmail' : 'settingsEmailOff'}
-                  onClick={() => (window.location.href = 'mailto:chinemeremchigbo@outlook.com')}
-                ></button>
-              </div>
-              <div className="grid-item">
-                {' '}
-                <button
-                  aria-label="Github"
-                  className={props.isSettings ? 'settingsGithub' : 'settingsGithubOff'}
-                  onClick={() => window.open('https://github.com/chinemeremchigbo', '_blank')}
-                ></button>
-              </div>
-              <div className="grid-item">
-                {' '}
-                <button
-                  aria-label="LinkedIn"
-                  className={props.isSettings ? 'settingsLinkedIn' : 'settingsLinkedInOff'}
-                  onClick={() => window.open('https://www.linkedin.com/in/chinemeremchigbo/', '_blank')}
-                ></button>
-              </div>
-              <div className="grid-item">
-                {' '}
-                <button
-                  aria-label="VSCO"
-                  className={props.isSettings ? 'settingsVSCO' : 'settingsVSCOOff'}
-                  onClick={() => window.open('https://vsco.co/chinemeremchigbo/gallery', '_blank')}
-                ></button>
-              </div>
-              <div className="grid-item">
-                {' '}
-                <button
-                  aria-label="Youtube"
-                  className={props.isSettings ? 'settingsYoutube' : 'settingsYoutubeOff'}
-                  onClick={() => window.open('https://youtube.com/channel/UCsTrMTQ90BXwjRGkim9CyhQ', '_blank')}
-                ></button>
-              </div>
-              <div className="grid-item"></div>
-              <div className="grid-item"></div>
+              <button
+                aria-label="Devpost"
+                className={props.isSettings ? 'settingsDevpost' : 'settingsDevpostOff'}
+                onClick={() => window.open('https://devpost.com/ChinemeremChigbo', '_blank')}
+              ></button>
+              <button
+                aria-label="Email"
+                className={props.isSettings ? 'settingsEmail' : 'settingsEmailOff'}
+                onClick={() => (window.location.href = 'mailto:chinemeremchigbo@outlook.com')}
+              ></button>{' '}
+              <button
+                aria-label="Github"
+                className={props.isSettings ? 'settingsGithub' : 'settingsGithubOff'}
+                onClick={() => window.open('https://github.com/chinemeremchigbo', '_blank')}
+              ></button>{' '}
+              <button
+                aria-label="LinkedIn"
+                className={props.isSettings ? 'settingsLinkedIn' : 'settingsLinkedInOff'}
+                onClick={() => window.open('https://www.linkedin.com/in/chinemeremchigbo/', '_blank')}
+              ></button>{' '}
+              <button
+                aria-label="VSCO"
+                className={props.isSettings ? 'settingsVSCO' : 'settingsVSCOOff'}
+                onClick={() => window.open('https://vsco.co/chinemeremchigbo/gallery', '_blank')}
+              ></button>{' '}
+              <button
+                aria-label="Youtube"
+                className={props.isSettings ? 'settingsYoutube' : 'settingsYoutubeOff'}
+                onClick={() => window.open('https://youtube.com/channel/UCsTrMTQ90BXwjRGkim9CyhQ', '_blank')}
+              ></button>
             </div>
           </div>
         </div>
