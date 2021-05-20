2fa3179f78db2a58f5b402d9591d4e1c718969e2
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2021-05-20 13:24:19 -0400

    Revamped backgrounds and commented on game

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Game.scss';
 import $ from 'jquery';
 import { Link, Redirect, useHistory } from 'react-router-dom';
+import './style.scss';
 
 export default function Game() {
   $(function () {
+    //Declares the game canvas
     let canvas = document.getElementById('canvas'),
       ctx = canvas.getContext('2d');
+    //Gets users screensize
     let screenHeight = $(window).height();
     let screenWidth = $(window).width();
-
+    /*Prevents the portal page changing from occuring more than once
+    (really, twice in practice due to game performance) if turned off
+    page loading is even slower since it is requested too many times
+    */
     let navigateOnce = true;
+    //Makes the actual canvas huge, but the effective canvas small (so users can't easily see the edge of the canvas)
     let screenSizePadding = 4000;
+    //Canvas sizing
     canvas.width = 1880 + screenSizePadding;
     canvas.height = 700 + screenSizePadding;
     let x = canvas.width / 2,
       y = canvas.height / 2,
+      //Movement for the parallax background
       xSlow = canvas.width / 2,
       ySlow = canvas.height / 2,
+      //To ramp up and slow down character movement
       velY = 0,
       velX = 0,
+      //Speed cap for x and y directions
       maxSpeedX = 3,
       maxSpeedY = 2,
+      //Speed increase factor
       speedIncrement = 0.1,
-      friction = 0.99,
+      /*Allows character to slow down instead of stopping immediately
+      the lower, the faster the stopping*/
+      friction = 0.95,
+      //Used to stop the back parallax movement at the same time as the front
       backgroundMovementX = true,
       backgroundMovementY = true,
-      // boostTimeout = 1000,
-      // boostReady = false,
+      //To collect which key was pressed
       keys = [];
-
+    //Character Spritesheet
     var PlayerFast = [];
     var FastFrame = 0;
     var PlayerHurt = [];
@@ -40,7 +54,6 @@ export default function Game() {
     var RushFrame = 0;
     var PlayerSwimming = [];
     var SwimmingFrame = 0;
-    var moveOnce = true;
     var Fast = [
       './player/Fast/1.png',
       './player/Fast/2.png',
@@ -81,53 +94,14 @@ export default function Game() {
       './player/Swimming/6.png',
       './player/Swimming/7.png',
     ];
-    var once = false;
-
+    //Detects when key is first pressed and released for player movement
     document.body.addEventListener('keydown', function (e) {
       keys[e.key] = true;
     });
     document.body.addEventListener('keyup', function (e) {
       keys[e.key] = false;
     });
-    // document.body.addEventListener(
-    //   'touchstart',
-    //   function (e) {
-    //     var mousePos = getMousePos(canvas, e);
-    //     if (isInside(mousePos, rectRight)) {
-    //       keys['ArrowRight'] = true;
-    //     }
-    //     if (isInside(mousePos, rectLeft)) {
-    //       keys['ArrowLeft'] = true;
-    //     }
-    //     if (isInside(mousePos, rectUp)) {
-    //       keys['ArrowUp'] = true;
-    //     }
-    //     if (isInside(mousePos, rectDown)) {
-    //       keys['ArrowDown'] = true;
-    //     }
-    //   },
-    //   false
-    // );
-    // document.body.addEventListener(
-    //   'touchend',
-    //   function (e) {
-    //     var mousePos = getMousePos(canvas, e);
-    //     alert(mousePos);
-    //     if (isInside(mousePos, rectRight)) {
-    //       keys['ArrowRight'] = false;
-    //     }
-    //     if (isInside(mousePos, rectLeft)) {
-    //       keys['ArrowLeft'] = false;
-    //     }
-    //     if (isInside(mousePos, rectUp)) {
-    //       keys['ArrowUp'] = false;
-    //     }
-    //     if (isInside(mousePos, rectDown)) {
-    //       keys['ArrowDown'] = false;
-    //     }
-    //   },
-    //   false
-    // );
+    //Adds Swipe touch functionality for mobile
     var touchStartPositionY;
     var touchStartPositionX;
     $(document).bind('touchstart', function (e) {
@@ -161,70 +135,10 @@ export default function Game() {
         }
       }
     });
-    // canvas.addEventListener(
-    //   'click',
-    //   function (e) {
-    //     var mousePos = getMousePos(canvas, e);
-    //     debugger;
-    //     if (isInside(mousePos, rect)) {
-    //       lastDownTarget = e.target;
-    //       keys['ArrowRight'] = true;
-    //     }
-    //   },
-    //   false
-    // );
-    // canvas.addEventListener(
-    //   'click',
-    //   function (evt) {
-    //     var mousePos = getMousePos(canvas, evt);
-    //     debugger;
-    //     if (isInside(mousePos, rect)) {
-    //       alert('clicked inside rect');
-    //     } else {
-    //       alert('clicked outside rect');
-    //     }
-    //   },
-    //   false
-    // );
-    function getMousePos(canvas, event) {
-      var rect = canvas.getBoundingClientRect();
-      return {
-        x: event.clientX - rect.left,
-        y: event.clientY - rect.top,
-      };
-    }
-    function isInside(pos, rect) {
-      return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y;
-    }
-
-    var rectUp = {
-      x: 0,
-      y: 0,
-      width: screenWidth,
-      height: screenHeight / 4,
-    };
-    var rectDown = {
-      width: screenWidth,
-      height: screenHeight / 4,
-      x: 0,
-      y: screenHeight - rectUp.height,
-    };
-    var rectRight = {
-      x: screenWidth - screenWidth / 2,
-      y: rectUp.height,
-      width: screenWidth / 2,
-      height: screenHeight - rectUp.height - rectDown.height,
-    };
-    var rectLeft = {
-      x: 0,
-      y: rectUp.height,
-      width: screenWidth / 2,
-      height: screenHeight - rectUp.height - rectDown.height,
-    };
 
     function Update() {
       requestAnimationFrame(Update);
-
+      //Updates sprite each frame
       PlayerFast[FastFrame] = new Image();
       PlayerFast[FastFrame].src = Fast[FastFrame];
       PlayerHurt[HurtFrame] = new Image();
@@ -261,11 +175,13 @@ export default function Game() {
           velX -= speedIncrement;
         }
       }
+      //Increases x and y velocity in accordance to friction
       velY *= friction;
       y += velY;
 
       velX *= friction;
       x += velX;
+      //Slower, parallax movement for background
       if (backgroundMovementX) {
         xSlow += velX * 0.5;
       }
@@ -273,6 +189,7 @@ export default function Game() {
         ySlow += velY * 0.5;
       }
 
+      //Stops movement at effective canvas limits
       if (x >= canvas.width - screenSizePadding / 2) {
         x = canvas.width - screenSizePadding / 2;
         backgroundMovementX = false;
@@ -292,61 +209,29 @@ export default function Game() {
       } else {
         backgroundMovementY = true;
       }
-
-      ctx.clearRect(0, 0, canvas.width, canvas.width);
+      /*Drawing the Background, Midground, and Foreground for the game
+      these are positioned in the center of the users screen and need to move in the opposite direction that
+      the user presses (the x and y in the 2nd and 3rd draw image) the first draw image is stationary.
+      Keep in mind, that the character does not actually move, just the background and foreground.
+      */
       var Background = new Image();
       Background.src = 'Background.png';
-      var MiddleGround = new Image();
-      MiddleGround.src = 'MiddleGround.png';
-      var GameBackground = new Image();
-      GameBackground.src = 'GameBackground.png';
+      var Midground = new Image();
+      Midground.src = 'Midground.png';
+      var Foreground = new Image();
+      Foreground.src = 'Foreground.png';
       ctx.drawImage(Background, screenWidth / 2 - Background.width / 2, screenHeight / 2 - Background.height / 2);
-      // ctx.fillStyle = '#0d1a20';
-      // ctx.fillRect(
-      //   canvas.width / 2 - x - GameBackground.width / 2 + screenWidth / 2 + GameBackground.width - 10,
-      //   canvas.height / 2 - y - GameBackground.height / 2 + screenHeight / 2,
-      //   2500,
-      //   GameBackground.height
-      // );
-      // ctx.fillRect(
-      //   0,
-      //   canvas.height / 2 - y - GameBackground.height / 2 + screenHeight / 2 + GameBackground.height,
-      //   GameBackground.width + 2490,
-      //   GameBackground.height
-      // );
-      // ctx.fillRect(
-      //   canvas.width / 2 - x - GameBackground.width / 2 + screenWidth / 2 - GameBackground.width,
-      //   canvas.height / 2 - y - GameBackground.height / 2 + screenHeight / 2,
-      //   GameBackground.width,
-      //   GameBackground.height
-      // );
-      // ctx.fillRect(
-      //   0,
-      //   canvas.height / 2 - y - GameBackground.height / 2 + screenHeight / 2 - GameBackground.height,
-      //   GameBackground.width + 2490,
-      //   GameBackground.height
-      // );
       ctx.drawImage(
-        MiddleGround,
-        canvas.width / 2 - xSlow - MiddleGround.width / 2 + screenWidth / 2,
-        canvas.height / 2 - ySlow - MiddleGround.height / 2 + screenHeight / 2
+        Midground,
+        canvas.width / 2 - xSlow - Midground.width / 2 + screenWidth / 2,
+        canvas.height / 2 - ySlow - Midground.height / 2 + screenHeight / 2
       );
       ctx.drawImage(
-        GameBackground,
-        canvas.width / 2 - x - GameBackground.width / 2 + screenWidth / 2,
-        canvas.height / 2 - y - GameBackground.height / 2 + screenHeight / 2
+        Foreground,
+        canvas.width / 2 - x - Foreground.width / 2 + screenWidth / 2,
+        canvas.height / 2 - y - Foreground.height / 2 + screenHeight / 2
       );
-      // if (screenWidth < 1024) {
-      //   ctx.rect(rectUp.x, rectUp.y, rectUp.width, rectUp.height);
-      //   ctx.rect(rectDown.x, rectDown.y, rectDown.width, rectDown.height);
-      //   ctx.rect(rectRight.x, rectRight.y, rectRight.width, rectRight.height);
-      //   ctx.rect(rectLeft.x, rectLeft.y, rectLeft.width, rectLeft.height);
-      //   ctx.fillStyle = 'rgba(225,225,225,0.5)';
-      //   ctx.fill();
-      //   ctx.stroke();
-      //   ctx.closePath();
-      // }
-      //left portal
+      //Left portal (Home)
       if (
         x < canvas.width / 2 - 939 &&
         y > canvas.height / 2 - 60 &&
@@ -356,7 +241,7 @@ export default function Game() {
         navigateOnce = false;
         window.location.href = '/';
       }
-      //right portal (Contact)
+      //Right portal (Contact)
       if (
         x > canvas.width / 2 + 939 &&
         y > canvas.height / 2 - 40 &&
@@ -366,7 +251,7 @@ export default function Game() {
         navigateOnce = false;
         window.location.href = '/contact';
       }
-      //bottom portal (About)
+      //Bottom portal (About)
       if (
         x > canvas.width / 2 + 105 &&
         x < canvas.width / 2 + 185 &&
@@ -376,7 +261,7 @@ export default function Game() {
         navigateOnce = false;
         window.location.href = '/about';
       }
-      //top portal (Experience)
+      //Top portal (Experience)
       if (
         x > canvas.width / 2 - 15 &&
         x < canvas.width / 2 + 70 &&
@@ -389,6 +274,7 @@ export default function Game() {
     }
     Update();
 
+    //Function to draw consecutive character frames
     function drawGif(arr, x, y, scalex, scaley, rotate, factor, changespeed) {
       if (!factor) {
         factor = 1;
@@ -439,11 +325,68 @@ export default function Game() {
     }
     requestAnimationFrame(PlayerMovement);
   });
+  // Player following with fishies
+  // const secondaryCursor = React.useRef(null);
+  // const mainCursor = React.useRef(null);
+  // const positionRef = React.useRef({
+  //   mouseX: 0,
+  //   mouseY: 0,
+  //   destinationX: 0,
+  //   destinationY: 0,
+  //   distanceX: 0,
+  //   distanceY: 0,
+  //   key: -1,
+  // });
+
+  // React.useEffect(() => {
+  //   document.addEventListener('mousemove', (event) => {
+  //     const { clientX, clientY } = event;
+
+  //     const mouseX = clientX;
+  //     const mouseY = clientY;
+
+  //     positionRef.current.mouseX = mouseX - secondaryCursor.current.clientWidth / 2;
+  //     positionRef.current.mouseY = mouseY - secondaryCursor.current.clientHeight / 2;
+  //     mainCursor.current.style.transform = `translate3d(${mouseX - mainCursor.current.clientWidth / 2}px, ${
+  //       mouseY - mainCursor.current.clientHeight / 2
+  //     }px, 0)`;
+  //   });
+
+  //   return () => {};
+  // }, []);
+
+  // React.useEffect(() => {
+  //   const followMouse = () => {
+  //     positionRef.current.key = requestAnimationFrame(followMouse);
+  //     const { mouseX, mouseY, destinationX, destinationY, distanceX, distanceY } = positionRef.current;
+  //     if (!destinationX || !destinationY) {
+  //       positionRef.current.destinationX = mouseX;
+  //       positionRef.current.destinationY = mouseY;
+  //     } else {
+  //       positionRef.current.distanceX = (mouseX - destinationX) * 0.02;
+  //       positionRef.current.distanceY = (mouseY - destinationY) * 0.02;
+  //       if (Math.abs(positionRef.current.distanceX) + Math.abs(positionRef.current.distanceY) < 0.01) {
+  //         positionRef.current.destinationX = mouseX;
+  //         positionRef.current.destinationY = mouseY;
+  //       } else {
+  //         positionRef.current.destinationX += distanceX;
+  //         positionRef.current.destinationY += distanceY;
+  //       }
+  //     }
+  //     secondaryCursor.current.style.transform = `translate3d(${destinationX}px, ${destinationY}px, 0)`;
+  //   };
+  //   followMouse();
+  // }, []);
+
   return (
-    <div className="gameBackground">
-      <div className="game">
-        <canvas id="canvas"></canvas>
+    <div>
+      <canvas id="canvas" className="canvas"></canvas>
+      {/* <div className="main-cursor " ref={mainCursor}>
+        <div className="main-cursor-background"></div>
       </div>
+      <div className="secondary-cursor" ref={secondaryCursor}>
+        <div className="cursor-background"></div>
+      </div> */}
     </div>
   );
 }
