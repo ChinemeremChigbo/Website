4e22830b015e8cfc0391febbb1710d0e504f7e16
Chinemerem Chigbo <chinemeremchigbo@Outlook.com>
2022-01-17 14:09:32 -0500

    fixed broken swimming

--- src/components/shopify/Shopify.tsx
+++ src/components/shopify/Shopify.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./index.scss";
+import styles from "./index.module.scss";
 // Components
 import APOD from "./components/Apod";
 import Header from "./components/Header";
 
 export const Shopify = () => {
   return (
-    <div>
+    <div className={styles.shomepage}>
       <Header />
       <APOD />
     </div>
---
+++ src/components/shopify/actions/apod.js
@@ -0,0 +1,40 @@
+import { Dispatch, AnyAction } from "redux";
+import { typedAction } from "../types";
+import * as types from "../actionTypes";
+import apodService from "../services/apod";
+
+export type ApodAction = ReturnType<typeof typedAction>;
+
+export const getPictureOfTheDay = (date: string) => async (
+  dispatch: Dispatch<AnyAction>
+) => {
+  dispatch(typedAction(types.GET_PICTURE_OF_THE_DAY));
+  try {
+    const res = await apodService.getPictureOfTheDay(date);
+    localStorage.setItem("pictureOfTheDay", JSON.stringify(res.data));
+    dispatch(typedAction(types.GET_PICTURE_OF_THE_DAY_SUCCESS, res.data));
+  } catch (error) {
+      dispatch(
+        typedAction(types.GET_PICTURE_OF_THE_DAY_FAILURE, error.response.data)
+      );
+  }
+};
+
+export const getPreviousOrNextPicture = (date: string) => async (
+  dispatch: Dispatch<AnyAction>
+) => {
+  dispatch(typedAction(types.GET_PREVIOUS_OR_NEXT_DAY_PICTURE));
+  try {
+    const res = await apodService.getPictureOfTheDay(date);
+    dispatch(
+      typedAction(types.GET_PREVIOUS_OR_NEXT_DAY_PICTURE_SUCCESS, res.data)
+    );
+  } catch (error) {
+    dispatch(
+      typedAction(
+        types.GET_PREVIOUS_OR_NEXT_DAY_PICTURE_FAILURE,
+        error.response.data
+      )
+    );
+  }
+};
